dc0b117e01310c546d9ba802b24715d6e7cb5710
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-05 14:54:53 +0200

    ✨ ref on div to scroll on new message

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
+  <div ref="chatRef" class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
       <ChatBubble v-for="message in messages" :key="message.id" v-bind="message" />
@@ -11,12 +11,24 @@
 </template>
 
 <script setup lang="ts">
-import type { ChatMessage } from '@/interfaces/chat-message.interface';
+import { ref, watch } from 'vue';
 import ChatBubble from './ChatBubble.vue';
+import type { ChatMessage } from '@/interfaces/chat-message.interface';
 
 interface Props {
   messages: ChatMessage[];
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const chatRef = ref<HTMLDivElement | null>(null);
+
+watch(props.messages, () => {
+  setTimeout(() => {
+    chatRef.value?.scrollTo({
+      top: chatRef.value.scrollHeight,
+      behavior: 'smooth',
+    });
+  }, 100);
+});
 </script>
